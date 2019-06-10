a31b166c9ecff141e0fbb83307af9ccb489fc5ab
Pedro Martins <thedeadbr@gmail.com>
2019-06-10 19:15:17 -0300

    calculadora somando, subtraindo, multiplicando e dividindo

--- ConsoleApp1/calculadora/Program.cs
+++ ConsoleApp1/calculadora/Program.cs
@@ -12,20 +12,76 @@ namespace calculadora
         {
             while (true)
             {
-                //ler o valor de A
-                var a = lerEscrever("Digite o valor de A:");
-                //ler o valor de B
-                var b = lerEscrever("DIgite o valor de B:");
-                //verifica se A e B são numeros decimal, e converte para numeros decimal
-                if (decimal.TryParse(a, out decimal _a) && decimal.TryParse(b, out decimal _b))
+                string temp = lerEscrever("entre com a formula:");
+                string[]dados = temp.Split('+', '-', '/', '*');
+               
+                if (dados.Count() > 1)
                 {
-                 //mostra o valor da soma entre A e B
-                    Console.WriteLine(Somar(_a, _b));
-                }else {
-                    Console.WriteLine("Valores não são numeros inteiros");
+                    //ler o valor de A
+                    string a = dados[0];
+                    //ler o valor de B
+                    string b = dados[1];
+                    if (temp.Contains("+"))
+                    {
+                        if (decimal.TryParse(a, out decimal _a) && decimal.TryParse(b, out decimal _b))
+                        {
+                            //mostra o valor da soma entre A e B
+                            Console.WriteLine(Somar(_a, _b));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valores não são numeros inteiros");
+                        }
+
+                    }
+                    else if (temp.Contains("-"))
+                    {
+                        if (decimal.TryParse(a, out decimal _a) && decimal.TryParse(b, out decimal _b))
+                        {
+                            //mostra o valor da soma entre A e B
+                            Console.WriteLine(Subtrair(_a, _b));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valores não são numeros inteiros");
+                        }
+
+
+                    }
+                    else if (temp.Contains("*"))
+                    {
+                        if (decimal.TryParse(a, out decimal _a) && decimal.TryParse(b, out decimal _b))
+                        {
+                            //mostra o valor da soma entre A e B
+                            Console.WriteLine(Multiplicar(_a, _b));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valores não são numeros inteiros");
+                        }
+
+
+                    }
+                    else if (temp.Contains("/"))
+                    {
+                        if (decimal.TryParse(a, out decimal _a) && decimal.TryParse(b, out decimal _b))
+                        {
+                            //mostra o valor da soma entre A e B
+                            Console.WriteLine(Dividir(_a, _b));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Valores não são numeros inteiros");
+                        }
+
+
+                    }
                 }
+
                 Console.WriteLine("pressione qualquer tecla pra continuar"); Console.ReadLine();
             }
+
+
 
         }
         //função que ler e escreve mensagens na tela
@@ -39,6 +95,19 @@ namespace calculadora
         static string Somar(decimal a, decimal b)
         {
             return (a + b).ToString();
+        }
+
+        static string Subtrair(decimal a, decimal b)
+        {
+            return (a - b).ToString();
+        }
+        static string Multiplicar(decimal a, decimal b)
+        {
+            return (a * b).ToString();
+        }
+        static string Dividir(decimal a, decimal b)
+        {
+            return (a / b).ToString();
         }
     }
 }
