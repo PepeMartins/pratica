bbdd8e3601c359e666d26b73bcb856124b15107f
Pedro Martins <thedeadbr@gmail.com>
2019-09-05 19:05:30 -0300

    tutorial mvvm

---
+++ ConsoleApp1/tutorial mvc/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+
+namespace tutorial_mvc.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public int Volume
+        {
+            get { return GetValue<int>(nameof(Volume)); }
+            set { SetValue(value, nameof(Volume)); }
+        }
+    }
+}
